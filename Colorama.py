ad00a3fff523c3b19375e48a921364d1db2d5a6a
HarveyGW <45942623+HarveyGW@users.noreply.github.com>
2019-03-06 10:24:00 +0000

    Create Colorama.py

---
+++ Colorama.py
@@ -0,0 +1,18 @@
+import colorama
+from colorama import Fore as f
+colour = input("What colour do you want: (YELLOW, GREEN, BLUE, RED) ")
+message = input("What do you want to say? ")
+if colour == "YELLOW":
+  print(f.YELLOW)
+  print(message)
+elif colour == "RED":
+  print(f.RED)
+  print(message)
+elif colour == "BLUE":
+  print(f.BLUE)
+  print(message)
+elif colour == "GREEN":
+  print(f.GREEN)
+  print(message)
+else:
+  print("Not a colour!")
